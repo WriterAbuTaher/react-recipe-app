a3c2cfe8a8542a4fa87fcfcea45255182c8892c7
Abu Taher <writerabutaher@gmail.com>
2022-09-30 11:40:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
+
+  const APP_ID = "9f02b767";
+  const APP_KEY = "7d7ce0c92119f8517f43c0f053c1a91e";
+
+  const URL = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
     </div>
   );
 }
